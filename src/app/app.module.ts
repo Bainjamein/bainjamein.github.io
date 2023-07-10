d6ee2baff455ac9091fb2f9deb70cf1e721b58ca
Bainjamein <91478713+Bainjamein@users.noreply.github.com>
2023-07-10 14:50:07 +0200

    build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponentComponent } from './navbar-component/navbar-component.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { AboutComponentComponent } from './about-component/about-component.component';
+import { ProjectsComponentComponent } from './projects-component/projects-component.component';
+import { ContactComponentComponent } from './contact-component/contact-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponentComponent,
+    HomeComponentComponent,
+    AboutComponentComponent,
+    ProjectsComponentComponent,
+    ContactComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: '', component: HomeComponentComponent },
+      { path: 'about', component: AboutComponentComponent },
+      { path: 'projects', component: ProjectsComponentComponent },
+      { path: 'contact', component: ContactComponentComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
